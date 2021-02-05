ca2873ebb2305020f2970d88d3b5b31580e843fa
Igor Bedesqui <igorbedesqui@gmail.com>
2021-02-05 01:22:56 -0300

    build: added Staging notes

--- components/MapStuff/MapWrapper.tsx
+++ components/MapStuff/MapWrapper.tsx
@@ -3,7 +3,7 @@ const API_URL = "https://api.wheretheiss.at/v1/satellites/25544";
 export default function MapWrapper() {
   const { data, error } = useSWR(API_URL, {
     revalidateOnFocus: true,
-    refreshInterval: 7000,
+    refreshInterval: 12000,
   });
 
   useEffect(() => {
@@ -28,6 +28,14 @@ export default function MapWrapper() {
           <br />
           <button onClick={() => setLon(lon + 10)}>+10Lon</button>
           <button onClick={() => setLon(lon - 10)}>-10Lon</button>
+          <div className="bg-gray-800 font-mono leading-snug m-8 p-4 rounded-lg">
+            <B>STAGING ONLY:</B>
+            <br />
+            You can change the lat and lon on the go with the buttons above.
+            <br /> be aware that the Lat and Lon are being updated by an API
+            every 12 seconds give or take so don't be scared if your manual
+            changes are reverted out of nowhere
+          </div>
         </>
       )}
     </div>
@@ -37,3 +45,5 @@ export default function MapWrapper() {
 import { useEffect, useState } from "react";
 import useSWR from "swr";
 import MapChart from "./MapChart";
+
+import B from "../Bold";
