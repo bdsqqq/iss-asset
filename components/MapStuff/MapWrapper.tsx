449e8d2631e7a4097f9220aded1658f41ff1ea07
Igor Bedesqui <igorbedesqui@gmail.com>
2021-02-04 23:16:39 -0300

    feat: added communication with api and conditional rendering for buttons

--- components/MapStuff/MapWrapper.tsx
+++ components/MapStuff/MapWrapper.tsx
@@ -1,18 +1,39 @@
+const API_URL = "https://api.wheretheiss.at/v1/satellites/25544";
+
 export default function MapWrapper() {
-  const [lat, setLat] = useState(0);
-  const [lon, setLon] = useState(0);
+  const { data, error } = useSWR(API_URL, {
+    revalidateOnFocus: true,
+    refreshInterval: 7000,
+  });
+
+  useEffect(() => {
+    data && console.table(data);
+    error && console.log(error, "error");
+    if (data) {
+      setLat(data.latitude);
+      setLon(data.longitude);
+    }
+  }, [data, error]);
+
+  const [lat, setLat] = useState<undefined | number>();
+  const [lon, setLon] = useState<undefined | number>();
 
   return (
     <div>
       <MapChart lon={lon} lat={lat} />
-      <button onClick={() => setLat(lat + 10)}>+10Lat</button>
-      <button onClick={() => setLat(lat - 10)}>-10Lat</button>
-      <br />
-      <button onClick={() => setLon(lon + 10)}>+10Lon</button>
-      <button onClick={() => setLon(lon - 10)}>-10Lon</button>
+      {lat && lon && (
+        <>
+          <button onClick={() => setLat(lat + 10)}>+10Lat</button>
+          <button onClick={() => setLat(lat - 10)}>-10Lat</button>
+          <br />
+          <button onClick={() => setLon(lon + 10)}>+10Lon</button>
+          <button onClick={() => setLon(lon - 10)}>-10Lon</button>
+        </>
+      )}
     </div>
   );
 }
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import useSWR from "swr";
 import MapChart from "./MapChart";
