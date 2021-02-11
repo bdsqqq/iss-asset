ff0ed52c10e1045f5ee4c885a6cb923f611f0418
Igor Bedesqui <igorbedesqui@gmail.com>
2021-02-11 02:00:26 -0300

    feat: added Part and Wrapper

---
+++ components/partsStuff/Part.tsx
@@ -0,0 +1,40 @@
+interface PartProp {
+  key: string;
+  name: string;
+  nationality?: string;
+  role?: string;
+  description: string;
+}
+
+const Part: React.FC<PartProp> = ({
+  key,
+  name,
+  nationality,
+  role,
+  description,
+}) => {
+  return (
+    <div className="w-48" key={key}>
+      <p className="text-center font-bold text-2xl">{name}</p>
+      <Image
+        className="mx-auto"
+        src="/images/60x60.svg"
+        width={60}
+        height={60}
+      />
+      {role ? (
+        <p className="text-center font-light">{role}</p>
+      ) : (
+        <>
+          <br />
+        </>
+      )}
+      <br />
+      <p>{description}</p>
+    </div>
+  );
+};
+
+export default Part;
+
+import Image from "next/image";
---
+++ components/partsStuff/Wrapper.tsx
@@ -0,0 +1,50 @@
+const Wrapper = () => {
+  return (
+    <div className="flex overflow-x-auto">
+      {timeline.map((year, i) => {
+        return (
+          <article className="flex p-2 md:p-4">
+            <header
+              id={year.yearNumber}
+              className="t-writing-mode-vlr font-bold"
+            >
+              {year.yearNumber}
+            </header>
+            {year.months.map((month, j) => {
+              return (
+                <div className="flex mr-2">
+                  <p
+                    id={year.yearNumber + "_" + month.name}
+                    className="t-writing-mode-vlr mt-4"
+                  >
+                    — {month.name}
+                  </p>
+                  <div className="flex mt-4">
+                    {month.parts.map(
+                      ({ name, nationality, role, description }, k) => {
+                        return (
+                          <Part
+                            key={`part-${i}-${j}-${k}`}
+                            name={name}
+                            nationality={nationality}
+                            role={role}
+                            description={description}
+                          />
+                        );
+                      }
+                    )}
+                  </div>
+                </div>
+              );
+            })}
+          </article>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Wrapper;
+
+import Part from "./Part";
+import timeline from "./timeline";
