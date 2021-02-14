52dbf250fdef2f1f0c3b9e163970a96f6c4d9f14
Igor Bedesqui <igorbedesqui@gmail.com>
2021-02-14 03:22:20 -0300

    feat: removed staging utils

--- components/MapStuff/MapWrapper.tsx
+++ components/MapStuff/MapWrapper.tsx
@@ -18,28 +18,7 @@ export default function MapWrapper() {
   const [lat, setLat] = useState<undefined | number>();
   const [lon, setLon] = useState<undefined | number>();
 
-  return (
-    <div>
-      <Chart lon={lon} lat={lat} />
-      {lat && lon && (
-        <>
-          <button onClick={() => setLat(lat + 10)}>+10Lat</button>
-          <button onClick={() => setLat(lat - 10)}>-10Lat</button>
-          <br />
-          <button onClick={() => setLon(lon + 10)}>+10Lon</button>
-          <button onClick={() => setLon(lon - 10)}>-10Lon</button>
-          <div className="bg-gray-800 font-mono leading-snug m-8 p-4 rounded-lg">
-            <B>STAGING ONLY:</B>
-            <br />
-            You can change the lat and lon on the go with the buttons above.
-            <br /> be aware that the Lat and Lon are being updated by an API
-            every 12 seconds give or take so don't be scared if your manual
-            changes are reverted out of nowhere
-          </div>
-        </>
-      )}
-    </div>
-  );
+  return <Chart lon={lon} lat={lat} />;
 }
 
 import { useEffect, useState } from "react";
