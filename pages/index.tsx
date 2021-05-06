ddd0d4d8fb340ed5c72be062cbd683f3de65bc86
Igor Bedesqui <igorbedesqui@gmail.com>
2021-05-06 19:05:50 -0300

    feat: update main page, removed unecessary stuff

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,44 +4,18 @@ export default function Home() {
     <>
       <PageHead />
       <Container>
-        <section
-          className="relative w-full h-75vh bg-gray-900 text-gray-100"
-          id="hero"
+        <Band
+          dark
+          headline={{
+            bold: "01",
+            thin: "Wheres the iss?",
+          }}
         >
-          <div className="flex items-center justify-center w-full h-full absolute z-10">
-            <h1 className="font-bold uppercase text-2xl md:text-4xl text-center">
-              <span className="text-lg md:text-xl">{t("hero1")}</span> <br />
-              <br />
-              {t("hero2")} <br />{" "}
-              <span className="text-7xl md:text-8xl">{t("hero3")}</span>
-            </h1>
-          </div>
-          <div className="h-full w-full my-0 mx-auto">
-            <Image
-              priority
-              className="p-16-caralho md:p-24-caralho"
-              src="/images/iss-view.jpg"
-              layout="fill"
-              objectFit="cover"
-              objectPosition="right"
-            />
-            <div className="absolute h-full w-full bg-gray-800 bg-opacity-60" />
-          </div>
-        </section>
-        <Band dark gridless id="disclaimer">
-          <div className="w-full bg-gray-800  p-16 rounded-lg">
-            <span className="font-mono leading-snug">
-              <B>Staging only:</B>
-              <br />
-              Probably a lot of the copy and design specific stuff will change,
-              this is meant to show the general idea of this asset. <br />
-              Thanks for taking the time to look at it :)
-            </span>
-          </div>
+          <MapWrapper />
         </Band>
         <Band
           headline={{
-            bold: "01",
+            bold: "02",
             thin: "What is it?",
           }}
         >
@@ -62,10 +36,10 @@ export default function Home() {
             <B> into the infinity and beyond.</B>
           </p>
         </Band>
-        <Band
+        {/*<Band
           dark
           headline={{
-            bold: "02",
+            bold: "03",
             thin: "not your average lego set",
           }}
         >
@@ -82,29 +56,7 @@ export default function Home() {
         </Band>
         <Band dark padless gridless id="timeline">
           <Wrapper />
-        </Band>
-        <Band
-          dark
-          headline={{
-            bold: "03",
-            thin: "Wheres the iss?",
-            additional: (
-              <div className="my-4 mr-8">
-                currently it is in
-                <br />
-                <br />
-                <div className="bg-gray-800 font-mono leading-snug p-4 rounded-lg">
-                  <B>WIP:</B> trying to add the name of the place the iss is
-                  hovering over. currently strugling to find something that
-                  would be easy for internationalization and can be quickly
-                  updated with Lat and Lon
-                </div>
-              </div>
-            ),
-          }}
-        >
-          <MapWrapper />
-        </Band>
+        </Band>*/}
       </Container>
     </>
   );
