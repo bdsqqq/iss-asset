3ad0b47b0c78b6c60c54ae1da6f2aeacc0f7b607
Igor Bedesqui <igorbedesqui@gmail.com>
2021-05-06 21:14:06 -0300

    feat: add dynamic dark to footer depending on last band

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,4 +1,8 @@
 const Container: React.FC = ({ children }) => {
+  const childrenArray: any = Children.toArray(children);
+  const dark: boolean | undefined =
+    childrenArray[childrenArray.length - 1].props.dark;
+
   return (
     <div className="relative min-h-screen">
       <nav className="z-50 absolute flex justify-end items-start w-full px-4 py-4 my-0 md:pt-8 md:px-16 mx-auto">
@@ -12,13 +16,15 @@ const Container: React.FC = ({ children }) => {
       </nav>
       <div className="relative w-full">
         <main className="flex flex-col justify-center">{children}</main>
-        <Footer />
+        <Footer dark={dark} />
       </div>
     </div>
   );
 };
 
 export default Container;
+
+import { Children } from "react";
 
 import Footer from "./Footer";
 import ChangeLang from "./ChangeLang";
