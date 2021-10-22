456e176a4bd4ab1e3b5c5fb9cf507679c516a42d
Igor Bedesqui <igorbedesqui@gmail.com>
2021-10-22 17:44:30 -0300

    feat: configured stitches

---
+++ stitches.config.js
@@ -0,0 +1,27 @@
+import { slateDark, redDark, blueDark } from "@radix-ui/colors";
+import { createStitches } from "@stitches/react";
+
+export const {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  config,
+} = createStitches({
+  theme: {
+    colors: {
+      ...slateDark,
+      ...blueDark,
+      ...redDark,
+    },
+  },
+  media: {
+    bp1: "(min-width: 480px)",
+  },
+  utils: {
+    marginX: (value) => ({ marginLeft: value, marginRight: value }),
+  },
+});
