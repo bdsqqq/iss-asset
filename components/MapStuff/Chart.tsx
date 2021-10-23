80103627b582bc99fd8694448f7444a6bc8fdae2
Igor Bedesqui <igorbedesqui@gmail.com>
2021-10-22 22:05:32 -0300

    feat: stitched styles

--- components/MapStuff/Chart.tsx
+++ components/MapStuff/Chart.tsx
@@ -21,13 +21,8 @@ const Chart: React.FC<ChartProps> = ({ lon, lat }) => {
             rotate: [-lon, -lat, 0],
           }}
         >
-          <Graticule className="stroke-current text-gray-600" />
-          <Geographies
-            className="fill-current text-gray-700"
-            geography={geo}
-            stroke="#FFFFFF"
-            strokeWidth={0.5}
-          >
+          <StyledGraticule />
+          <StyledGeographies geography={geo} strokeWidth={0.5}>
             {({ geographies }) =>
               geographies.map((geo) => (
                 <Geography
@@ -37,11 +32,10 @@ const Chart: React.FC<ChartProps> = ({ lon, lat }) => {
                 />
               ))
             }
-          </Geographies>
+          </StyledGeographies>
 
           <Marker coordinates={[lon, lat]}>
-            <g
-              className="text-red-500 stroke-current"
+            <StyledG
               fill="none"
               strokeWidth="2"
               strokeLinecap="round"
@@ -50,7 +44,7 @@ const Chart: React.FC<ChartProps> = ({ lon, lat }) => {
             >
               <circle cx="12" cy="10" r="3" />
               <path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z" />
-            </g>
+            </StyledG>
           </Marker>
         </ComposableMap>
       )}
@@ -67,8 +61,23 @@ import {
   Graticule,
   Marker,
 } from "react-simple-maps";
+
+const StyledGeographies = styled(Geographies, {
+  fill: "$slate3",
+  stroke: "$slate12",
+});
+const StyledGraticule = styled(Graticule, {
+  stroke: "$slate9",
+});
+
+const StyledG = styled("g", {
+  stroke: "$red9",
+});
+
 import { default as geo } from "./wolrd-110m";
 
 import useMeasure from "react-use-measure";
 import { ResizeObserver } from "@juggle/resize-observer";
 import { useEffect } from "react";
+
+import { styled } from "stitches.config";
