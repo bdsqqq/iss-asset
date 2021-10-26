9d47ef669aff1dfcb79a67302712d1979f417047
Igor Bedesqui <igorbedesqui@gmail.com>
2021-10-25 21:49:10 -0300

    feat: updated styling, commented footer

--- components/Container.tsx
+++ components/Container.tsx
@@ -4,19 +4,19 @@ const Container: React.FC = ({ children }) => {
     childrenArray[childrenArray.length - 1].props.dark;
 
   return (
-    <div className="relative min-h-screen">
+    <div className="relative min-h-screen bg-slate1">
       <nav className="z-50 absolute flex justify-end items-start w-full px-4 py-4 my-0 md:pt-8 md:px-16 mx-auto">
         <div>
           <ChangeLang
-            generalClasses="p-1 sm:p-2 text-gray-100 lowercase"
+            generalClasses="p-1 sm:p-2 text-slate12 lowercase"
             activeClasses="font-bold"
-            inactiveClasses="text-gray-500 hover:text-gray-300 focus:text-gray-300"
+            inactiveClasses="text-slate11 hover:text-slate12 focus:text-slate12"
           />
         </div>
       </nav>
       <div className="relative w-full">
         <main className="flex flex-col justify-center">{children}</main>
-        <Footer dark={dark} />
+        {/* <Footer /> */}
       </div>
     </div>
   );
