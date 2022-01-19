3d356aac83e33f9f473bc35c60f66af29d42c3dd
Igor Bedesqui <igorbedesqui@gmail.com>
2022-01-18 22:44:01 -0300

    feat: implemented globe

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,42 @@
 export default function Home() {
   const { lat, lon } = useIssStore();
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
   const { t, lang } = useTranslation("home");
+
+  useEffect(() => {
+    let phi = 0;
+
+    const globe = createGlobe(canvasRef.current, {
+      devicePixelRatio: 2,
+      width: 300 * 2,
+      height: 300 * 2,
+      phi: 0,
+      theta: 0,
+      dark: 1,
+      diffuse: 1.2,
+      mapSamples: 16000,
+      mapBrightness: 6,
+      baseColor: [0.3, 0.3, 0.3],
+      markerColor: [0.1, 0.8, 1],
+      glowColor: [1, 1, 1],
+      markers: [
+        // longitude latitude
+        { location: [lon, lat], size: 0.05 },
+      ],
+      onRender: (state: any) => {
+        // Called on every animation frame.
+        // `state` will be an empty object, return updated params.
+        state.phi = phi;
+        phi += 0.01;
+      },
+    });
+
+    return () => {
+      globe.destroy();
+    };
+  }, []);
+
   return (
     <>
       <PageHead t={t} lang={lang} />
@@ -14,7 +49,7 @@ export default function Home() {
               overflow: "hidden",
             }}
           >
-            <MapWrapper />
+            <canvas ref={canvasRef} style={{ width: 300, height: 300 }} />
           </Box>
           <Box
             css={{
@@ -168,3 +203,6 @@ import Container from "../components/Container";
 import MapWrapper from "../components/MapStuff/MapWrapper";
 import { useIssStore } from "@/components/store/useIssStore";
 import { styled } from "stitches.config";
+import { useEffect, useRef } from "react";
+
+import createGlobe from "cobe";
