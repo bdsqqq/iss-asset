19ae6fb3d4669e48f13431f3725f287d9374072b
Igor Bedesqui <igorbedesqui@gmail.com>
2022-07-28 22:01:43 -0300

    refactor: no need for zustand if I have react query

--- components/store/useIssStore.tsx
+++
@@ -1,8 +0,0 @@
-import create from "zustand";
-
-export const useIssStore = create((set) => ({
-  lat: 0,
-  lon: 0,
-  setLatLon: (data: any) =>
-    set(() => ({ lat: data.latitude, lon: data.longitude })),
-}));
