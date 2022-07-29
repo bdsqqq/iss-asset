d419d4ccaa3fd50e6291f4b79b6b92457556db90
Igor Bedesqui <igorbedesqui@gmail.com>
2022-07-28 22:01:24 -0300

    feat: use vercel edge to cache instead of a overcomplicated solution

--- pages/api/issData.ts
+++ pages/api/issData.ts
@@ -1,30 +1,18 @@
-const fetchData = async (url: string) => {
-  const query = await fetch(url);
-  return await query.json();
+export default async () => {
+  const API_URL = "https://api.wheretheiss.at/v1/satellites/25544";
+  const { latitude, longitude } = await fetch(API_URL).then((res) =>
+    res.json()
+  );
+
+  return new Response(JSON.stringify({ latitude, longitude }), {
+    status: 200,
+    headers: {
+      "content-type": "application/json",
+      "Cache-Control": "public, s-maxage=1, stale-while-revalidate",
+    },
+  });
 };
 
-const API_URL = "https://api.wheretheiss.at/v1/satellites/25544";
-
-const getIssData = async (fetcher: any, API_URL: string) => {
-  const cacheKey = "issData";
-
-  let cacheEntry = await redis.get(cacheKey);
-
-  if (cacheEntry) {
-    let jsonCacheEntry: object = JSON.parse(cacheEntry);
-    return { ...jsonCacheEntry, source: "cache" };
-  }
-
-  let issData = await fetcher(API_URL);
-  redis.set(cacheKey, JSON.stringify(issData), "EX", 12);
-
-  return { ...issData, source: "api" };
+export const config = {
+  runtime: "experimental-edge",
 };
-
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  let issData = await getIssData(fetchData, API_URL);
-  res.status(200).json(issData);
-};
-
-import type { NextApiRequest, NextApiResponse } from "next";
-import redis from "../../lib/redis";
