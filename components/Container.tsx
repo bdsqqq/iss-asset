547c5ceb1ce2395663bcba31029b26dc350b13aa
Igor Bedesqui <igorbedesqui@gmail.com>
2022-09-21 19:31:05 -0300

    feat: remove translations

--- components/Container.tsx
+++ components/Container.tsx
@@ -7,11 +7,6 @@ const Container: React.FC = ({ children }) => {
         backgroundColor: "$slate1",
       }}
     >
-      <Nav>
-        <Box>
-          <ChangeLang />
-        </Box>
-      </Nav>
       <div className="relative w-full">
         <main className="flex flex-col justify-center">{children}</main>
         {/* <Footer /> */}
@@ -20,29 +15,6 @@ const Container: React.FC = ({ children }) => {
   );
 };
 
-const Nav = styled("nav", {
-  position: "absolute",
-  zIndex: "1",
-  right: "1rem",
-  top: "1rem",
-  padding: "0 0.5rem",
-
-  backgroundColor: "$slate1",
-
-  borderStyle: "solid",
-  borderWidth: "1px",
-  borderColor: "$slate7",
-  borderRadius: "4px",
-
-  "@supports(backdrop-filter: saturate(150%) blur(20px))": {
-    backdropFilter: "saturate(180%) blur(6px)",
-    background: "none",
-  },
-});
-
 export default Container;
 
-import { styled } from "stitches.config";
 import Box from "@/ui/Box";
-import Footer from "./Footer";
-import ChangeLang from "./ChangeLang";
--- next.config.js
+++
@@ -1,3 +0,0 @@
-const nextTranslate = require("next-translate");
-// if you ever need to add more configs, check out the documentation for next-translate on github
-module.exports = nextTranslate();
