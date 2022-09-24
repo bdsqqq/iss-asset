d337a2024006d0a8d3231c8abfd34fb5a2923eec
Igor Bedesqui <igorbedesqui@gmail.com>
2022-09-24 01:00:32 -0300

    fix: head in _document instead of _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,4 @@
 import "../styles/index.css";
-import Head from "next/head";
-import Script from "next/script";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
@@ -9,31 +7,6 @@ const queryClient = new QueryClient();
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <Head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-        <Script
-          strategy="afterInteractive"
-          data-website-id="e7afd50c-8f87-4a3a-a19e-b42c67e3c4a7"
-          src="https://umami.igorbedesqui.com/umami.js"
-        />
-      </Head>
       <Component {...pageProps} />
     </QueryClientProvider>
   );
---
+++ pages/_document.tsx
@@ -0,0 +1,43 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+
+          <script
+            async
+            defer
+            data-website-id="e7afd50c-8f87-4a3a-a19e-b42c67e3c4a7"
+            src="https://umami.igorbedesqui.com/umami.js"
+          ></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
